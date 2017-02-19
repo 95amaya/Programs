eacd81b75424cd06d7f43671a38b5c82d69ec3a0
95amaya <michael.amaya09@gmail.com>
2017-02-19 01:07:28 +0000

    parallel hello world

--- python_projects/HelloWorld.py
+++ python_projects/HelloWorld.py
@@ -1,1 +1,19 @@
-print("Hello World!");
+#!/usr/bin/env python
+"""
+Parallel HelloWorld using mpy4py python
+wrapper for mpi. To run, type command:
+mpiexec -n 1 python HelloWorld.py
+"""
+
+from mpi4py import MPI
+import sys
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+
+sys.stdout.write( 
+    "Hello Parallel World! I am process %d of %d on %s.\n"
+    % (rank, size, name))
+
+
