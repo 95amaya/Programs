fecdddf326a9f96f090d50f9de10f0c36f75f67a
95amaya <michael.amaya09@gmail.com>
2017-08-31 10:34:29 -0500

    Systems Programming Project 1 Start

---
+++ c_projects/SystemsProgramming/Assignments/1_Customers/ActionMenu.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# check for too few arguments, be careful of the spacing
+# if [ $# -lt 1 ]; then
+#     echo "usage: simpWhile number"
+#     echo "       where number is a number"
+#     exit 1
+# fi
+# sum=0
+# index=$1
+# # the spacing is important
+# while [ $index -gt 0 ]; do
+#     echo -ne "$index "
+#     if [ $index -ne 1 ]; then
+#         echo -ne "+ "
+#     fi
+#     ((sum=sum+index))
+#     ((index=index-1))
+# done
+# echo -ne "\n = $sum\n"
+
+echo "Enter one of the following actions or press CTRL-D to exit."
+echo "C - create a customer file"
+echo "P - accept a customer payment"
+echo "F - find customer by apartment number"
+
+read action
+echo "Action = $action"
