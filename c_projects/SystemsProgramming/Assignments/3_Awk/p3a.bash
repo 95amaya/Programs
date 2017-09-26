2d86dec1659d3fd39e1968bd5b27f7e1a50054c1
95amaya <michael.amaya9@gmail.com>
2017-09-26 09:38:59 -0500

    Assignment 3 Completed using GAWK

---
+++ c_projects/SystemsProgramming/Assignments/3_Awk/p3a.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Use gawk script to print last names first
+# Pipe into sort to sort by last names
+# Reverse the sorted strings to show first -> last name
+
+# gawk -f p3a.awk unsortedNames.txt | sort
+gawk -f p3a.awk unsortedNames.txt | sort | gawk -f p3a.awk > p3a.out
---
+++ c_projects/SystemsProgramming/Assignments/3_Awk/p3b.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Print Flight information using awk script
+gawk -f p3b.awk reservation.txt > p3b.out
